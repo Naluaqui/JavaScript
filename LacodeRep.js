c3b17e059beb76fcc440e893eee6ddcb1106ff81
Ana Luiza <ana.tibi@outlook.com>
2025-04-29 08:46:58 -0300

    Arrumando pastas

--- LacodeRep.js
+++ LacodeRep.js
@@ -1,4 +1,11 @@
 for(let i=0;i<5;i++){
 
     console.log(`Interação número: ${i}`);
+
+}
+
+let contador = 0;
+while(contador < 3){
+    console.log(`Interação número: ${contador}`);
+    contador++;
 }
